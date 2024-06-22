42aa0392c0ed753051ffdd57bb24b7fd8a06ecfb
zhiyuanhubj <zhiyuan.hu.bj@gmail.com>
2024-06-22 15:52:24 +0000

    修改部分配置

--- proj_less/my_sh/llama2-7b-1-warmup.sh
+++ proj_less/my_sh/llama2-7b-1-warmup.sh
@@ -10,7 +10,7 @@ export CUDA_VISIBLE_DEVICES=0
 
 # warm up training
 DATA_DIR=../data
-MODEL_PATH=/root/xinglin-data/dsw_data/pretrain_model/Llama-2-7b-hf
+MODEL_PATH=meta-llama/Llama-2-7b-hf
 PERCENTAGE=0.05
 DATA_SEED=3
 JOB_NAME=llama2-7b-p${PERCENTAGE}-lora-seed${DATA_SEED}
